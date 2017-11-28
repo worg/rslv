83085a1ef4d41049f8c9b59adcdaeedbb9b8d0c5
Hiram J. Pérez <hi@worg.xyz>
2017-11-28 13:32:39 -0600

    add tests for fetchInvoices

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
+	"time"
 )
 
 func TestResuelve(t *testing.T) {
@@ -16,6 +17,45 @@ func TestResuelve(t *testing.T) {
 			endDate = `2017-11-01`
 			id = `1`
 			So(main, ShouldPanicWith, ErrorInvalidRange)
+		})
+
+		Convey(`Dates must comply with the specified format`, func() {
+			startDate = `2017 Jan 01`
+			endDate = `2017 Jan 03`
+			id = `1`
+			So(main, ShouldPanicWith, ErrorInvalidFormat)
+		})
+
+		Convey(`When making requests`, func() {
+			startDate = `2017-01-01`
+			endDate = `2017-03-01`
+			id = `1`
+			start, _ := time.Parse(timeFmt, startDate)
+			end, _ := time.Parse(timeFmt, endDate)
+			var requestChan, invoiceChan chan int
+
+			reset := func() {
+				requestChan = make(chan int)
+				invoiceChan = make(chan int)
+				requestCount = 0
+				invoiceCount = 0
+			}
+			reset()
+
+			Convey(`Counters should start on zero`, func() {
+				So(requestCount, ShouldEqual, 0)
+				So(invoiceCount, ShouldEqual, 0)
+			})
+
+			Convey(`Should Increment the request counter`, func() {
+				go processIncrements(requestChan, invoiceChan)
+				fetchInvoices(id, start, end, requestChan, invoiceChan)
+				wg.Wait()
+
+				So(requestCount, ShouldEqual, 1)
+			})
+
+			Reset(reset)
 		})
 
 	})
