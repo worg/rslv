03e17bdffa848dbaa4e15768711049a954b9c4ac
Hiram J. Pérez <hi@worg.xyz>
2017-12-01 11:19:06 -0600

    handle request errors

--- cmd/requests.go
+++ cmd/requests.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -15,19 +16,35 @@ func FetchInvoices(id string, start, end time.Time) (int, error) {
 	endString := end.Format(timeFmt)
 
 	// Do the API call
-	resp, err := http.Get(fmt.Sprintf(requestFormat, id, startString, endString))
+	url := fmt.Sprintf(requestFormat, id, startString, endString)
+	resp, err := http.Get(url)
 	if err != nil {
-		panic(err) // we treat this errors as fatal
+		panic(err) // we treat this error as fatal
 	}
 
 	// Get the response body
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err) // we treat this errors as fatal
+		panic(err) // we treat this error as fatal
 	}
 
+	stringBody := strings.Replace(string(body), `"`, ``, -1) // remove quotes from response body
+
+	// fail on HTTP errors
+	if resp.StatusCode > 200 {
+		if stringBody == ErrorAPILimit {
+			panic(ErrorAPILimit)
+		}
+
+		panic(stringBody)
+	}
+
+	if debug {
+		// we spawn a goroutine to prevent IO blocking
+		go fmt.Println(`RQ:`, stringBody, url)
+	}
 	// try to get the count from the response body
-	count, err := strconv.Atoi(string(body))
+	count, err := strconv.Atoi(stringBody)
 	var e *strconv.NumError
 	if err != nil {
 		e = err.(*strconv.NumError)
